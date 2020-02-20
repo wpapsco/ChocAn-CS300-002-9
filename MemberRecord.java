2b6e45e7ff259be182b5635ef3a6c727186bbdfe
Michael <michaelfidds@gmail.com>
2020-02-19 18:05:48 -0800

    Added MemberRecord class, data storage class for a member

---
+++ MemberRecord.java
@@ -0,0 +1,11 @@
+
+
+public class MemberRecord {
+    int ID;
+    String name;
+    boolean status; //false if suspended, true if valid
+    String address;
+    String city;
+    String state;
+    String zip;
+}
