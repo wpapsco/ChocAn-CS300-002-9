9242b8393d5d15e254c67f0713b6d87e7dcd4c17
root <root@DESKTOP-6HQTGPJ.localdomain>
2020-02-27 02:29:28 -0800

    refactored date parsing to use SimpleDateFormat

--- src/com/ICNH/chocan/Utilities.java
+++ src/com/ICNH/chocan/Utilities.java
@@ -1,9 +1,25 @@
 package com.ICNH.chocan;
 
+import java.util.Scanner;
+
 public class Utilities {
     //clears the standard output device by printing a bunch of newlines
     public static void clearConsole() {
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
                 "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+
+    public static boolean confirm() {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.print("(Y/N): ");
+            char yn = in.nextLine().toLowerCase().charAt(0);
+            if (yn == 'y') {
+                return true;
+            }
+            if (yn == 'n') {
+                return false;
+            }
+        }
     }
 }
